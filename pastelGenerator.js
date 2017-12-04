09f344bb3c08f9d2dd55d1eb23993d72259088dd
Eduardo Escoto <eduardo@escoto.me>
2017-12-04 14:49:33 -0800

    Improved color generation algorithm to allow for more color variety by allowing for variation in lightness. Todo: Check to see how saturation affects pastel generation, and also generate a color that varies from the color created right before it.

--- pastelGenerator.js
+++ pastelGenerator.js
@@ -10,6 +10,15 @@ module.exports = {
     }
 }
 
+function saveColor(color) {
+    let Jimp = require('jimp');
+    let image = new Jimp(1200, 900, color.hex.raw);
+    image.quality(100);
+    image.write('image.png', () => {
+        console.log('complete! Created color: ', color);
+    });
+}
+
 function addColorToData(color) {
     let fs = require('fs');
     fs.readFile('./pastelsCreated.json', (err, data) => {
@@ -35,24 +44,49 @@ function doesColorExist(color) {
     return false;
 }
 
+function getRandomLightnessInRange(min, max) {
+    let lightness = Math.random();
+    while (lightness < min || lightness > max) {
+        console.log("lightness out of range, retrying...", lightness);
+        lightness = Math.random();
+    }
+    return lightness.toFixed(3);;
+}
+
 function createColor() {
     const hue = Math.floor(Math.random() * 360);
+    const hueData = {
+        string: hue.toString(),
+        value: hue
+    }
     const saturation = "100%";
-    const lightness = "87.5%";
-    const rgb = hslToRgb(hue, 1, .875);
+    const saturationData = {
+        string: saturation,
+        value: 1
+    }
+    const pastelRange = {
+        min: .775,
+        max: .925,
+    }
+    const lightness = getRandomLightnessInRange(pastelRange.min, pastelRange.max);
+    const lightnessData = {
+        value: parseInt(lightness * 1000) / 1000,
+        string: ((lightness * 100).toFixed(1)).toString() + "%"
+    }
+    const rgb = hslToRgb(hueData.value, saturationData.value, lightnessData.value);
 
     const hex = {
         red: toHex(rgb.red),
         blue: toHex(rgb.blue),
         green: toHex(rgb.green),
         alpha: "FF",
-        combined:("#" + toHex(rgb.red) + toHex(rgb.green) + toHex(rgb.blue)).toUpperCase(),
+        combined: ("#" + toHex(rgb.red) + toHex(rgb.green) + toHex(rgb.blue)).toUpperCase(),
         raw: parseInt("0x" + toHex(rgb.red) + toHex(rgb.green) + toHex(rgb.blue) + "FF", 16)
     }
     const hsl = {
-        hue,
-        saturation,
-        lightness
+        hueData,
+        saturationData,
+        lightnessData
     };
     let color = {
         hsl,
@@ -78,7 +112,11 @@ function hslToRgb(hue, sat, light) {
     red = Math.floor(hueToRgb(t1, t2, hue + 2) * 255);
     green = Math.floor(hueToRgb(t1, t2, hue) * 255);
     blue = Math.floor(hueToRgb(t1, t2, hue - 2) * 255);
-    return {red, green, blue};
+    return {
+        red,
+        green,
+        blue
+    };
 }
 
 function toHex(n) {
@@ -106,3 +144,4 @@ function hueToRgb(t1, t2, hue) {
         return t1;
     }
 }
+saveColor(createColor());
